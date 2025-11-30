44694f4f0f658a1cf562d01cff8e1cd5c5c4b29d
Emis666 <emiscakah6@gmail.com>
2025-11-30 05:22:48 +0100

    Update ChronotypeDisplay.js

--- src/components/ChronotypeDisplay.js
+++ src/components/ChronotypeDisplay.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Owl from "../assets/owl.svg"; // path to your SVG
+import Owl from "./Owl"; // import the SVG component
 import "./ChronotypeDisplay.css";
 
 function ChronotypeDisplay() {
   return (
     <div className="chronotype-container">
-      <img src={Owl} alt="Owl" className="chronotype-owl" />
+      <Owl width={100} height={100} />
       <div className="chronotype-message">Your chronotype is an Owl!</div>
     </div>
   );
